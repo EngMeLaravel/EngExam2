f9bd2dda494d5afec6396c5207b166dd1e564801
lcnguyen181000 <caol639@gmail.com>
2019-12-22 23:30:04 +0700

    trang chu 1

---
+++ resources/views/blocks/footer.blade.php
@@ -0,0 +1,34 @@
+<div id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h5>Thông tin</h5>
+                <a href="">Về Mazii</a><br>
+                <a href="">Trợ giúp</a><br>
+                <a href="">Báo lỗi</a><br>
+                <a href="">Điều khoản sử dụng</a>
+            </div>
+            <div class="col-md-4">
+                <h5>Sản phẩm khác</h5>
+                <a href="">Học từ vựng thông minh Minder.</a>
+                <p> Giúp bạn ghi nhớ từ vựng hiệu quả.</p>
+            </div>
+            <div class="col-md-4">
+                <a href="">Easy Japanese</a>
+                <p>Học tiếng Nhật qua đọc báo</p>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <p class="text-center">
+                    Giấy phép số: 67/GP-BTTTT do Bộ Thông tin và Truyền thông cấp ngày 02 tháng 02 năm 2018;
+                    Cơ quan chủ quản: Công ty TNHH Công nghệ EUP – Phòng 403, toà nhà số 01 ngõ 120, Trường Chinh,
+                    Phương Mai, Đống Đa, Hà Nội
+                    ĐT: 0973 385 204
+                    Người chịu trách nhiệm: Ông Nguyễn Văn Trình.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/blocks/header.blade.php
@@ -0,0 +1,35 @@
+<div id="header">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">Hidden brand</a>
+            <ul class="navbar-nav mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#"><img src="{{ asset("frontend/images/header/translate.png")  }}" alt="Error"> Dịch <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><img src="{{ asset("frontend/images/header/shared_library.png")  }}" alt="Error"> Thư viện chung</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><img src="{{ asset("frontend/images/header/my_library.png")  }}" alt="Error"> Kho cá nhân</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><img src="{{ asset("frontend/images/header/shared_library.png")  }}" alt="Error"> Liên hệ</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="" class="nav-link">Đăng nhập</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link m-0">|</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">Đăng kí</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
