924417cb448417b2b629a252dd7e1e7bb11ce56b
Xuan Thuong <maixuanthuong1610@gmail.com>
2019-12-28 22:26:08 +0700

    alter table users

---
+++ database/migrations/2019_12_28_152018_alter_table_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->char('phone')->nullable();
+            $table->string('avatar')->nullable();
+            $table->tinyInteger('active')->default(1)->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('phone');
+            $table->dropColumn('avatar');
+            $table->dropColumn('active');
+        });
+    }
+}
