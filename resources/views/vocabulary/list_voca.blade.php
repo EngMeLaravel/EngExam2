a2d3752a3409b8befb56938af00e49cbf7f80c13
Xuan Thuong <maixuanthuong1610@gmail.com>
2020-01-11 01:34:53 +0700

    123

--- resources/views/vocabulary/list_voca.blade.php
+++ resources/views/vocabulary/list_voca.blade.php
@@ -6,33 +6,38 @@
     .mot_tu{
         padding: 10px;
     }
-    .mot_tu .list-group .list-group-item h4{
+    .mot_tu h4{
         font-weight: normal;
         text-transform: unset;
     }
-    .mot_tu .list-group .list-group-item h4 span{
+    .mot_tu h4 span{
         font-weight: 400;
         font-size: 16px;
     }
-    .mot_tu .list-group .list-group-item h4 span.accent_us{
-        color: red;
-    }
-    .mot_tu .list-group .list-group-item h4 span.accent_uk{
-        color: blue;
+    .mot_tu .row {
+        border: 1px solid #ccc;
+        height: auto;
+        padding: 8px;
+        border-radius: 5px;
     }
 </style>
 <div id="voca">
     <div class="mot_tu">
         @if(isset($vocabularies))
         @foreach($vocabularies as $vocabulary)
-        <div class="list-group">
-            <a href="{{ route('get_detail_voca.public_lib.index', [$vocabulary->cate_id,$vocabulary->subcate_id,$vocabulary->id]) }}" class="list-group-item">
-                <div>
-                    <h4 class="list-group-item-heading">{{ $vocabulary->voca_name }} <span class="text-danger">['{{ isset($vocabulary->vocatype->type_name) ? $vocabulary->vocatype->type_name : '[N\A]' }}']</span><span> /{{ $vocabulary->voca_spell }}/  : {{ $vocabulary->voca_mean }}</span> </h4>
-                    <p class="list-group-item-text">{{ $vocabulary->voca_example_en }}</p>
+            <div class="row">
+                <div class="col-xs-11">
+                    <a href="{{ route('get_detail_voca.public_lib.index', [$vocabulary->cate_id,$vocabulary->subcate_id,$vocabulary->id]) }}">
+                        <div>
+                            <h4 class="list-group-item-heading">{{ $vocabulary->voca_name }} <span class="text-danger">['{{ isset($vocabulary->vocatype->type_name) ? $vocabulary->vocatype->type_name : '[N\A]' }}']</span><span> /{{ $vocabulary->voca_spell }}/  : {{ $vocabulary->voca_mean }}</span> </h4>
+                            <p class="list-group-item-text">{{ $vocabulary->voca_example_en }}</p>
+                        </div>
+                    </a>
                 </div>
-            </a>
-        </div>
+                <div class="col-xs-1">
+                    <a href="{{ route('add_bookmark.my_lib.index', $vocabulary->id) }}" title="Thêm vào thư viện riêng"><i class="fa fa-plus"></i></a>
+                </div>
+            </div>
         @endforeach
         @endif
     </div>
